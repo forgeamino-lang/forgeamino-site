dc810095003585752af671c7cbd3ffcb037da2c2
forgeamino-lang <forgeamino@gmail.com>
2026-04-14 07:41:31 -0500

    SEO: add order/layout.js with noindex to block search indexing

---
+++ app/order/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: 'Order',
+  robots: {
+    index: false,
+    follow: false,
+  },
+}
+
+export default function OrderLayout({ children }) {
+  return children
+}
