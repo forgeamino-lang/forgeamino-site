e3f01ea7a8182bd6f520f30b1b230dbc439416bf
forgeamino-lang <forgeamino@gmail.com>
2026-04-14 06:36:39 -0500

    SEO: update robots.js to point to forgeamino.com

--- app/robots.js
+++ app/robots.js
@@ -1,13 +1,13 @@
 export default function robots() {
-  return {
-    rules: [
-      {
-        userAgent: '*',
-        allow: '/',
-        disallow: ['/api/', '/cart', '/checkout', '/order/'],
-      },
-    ],
-    sitemap: 'https://www.forgeamino.us/sitemap.xml',
-    host: 'https://www.forgeamino.us',
-  }
+    return {
+          rules: [
+            {
+                      userAgent: '*',
+                      allow: '/',
+                      disallow: ['/api/', '/cart', '/checkout', '/order/'],
+            },
+                ],
+          sitemap: 'https://www.forgeamino.com/sitemap.xml',
+          host: 'https://www.forgeamino.com',
+    }
 }
