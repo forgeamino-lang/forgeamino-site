9703f3119c505346fd3993c06f5ec2d818c13d98
forgeamino-lang <forgeamino@gmail.com>
2026-04-14 13:38:01 -0500

    Add account layout with noindex metadata

---
+++ app/account/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+    title: 'My Account | Forge Amino',
+    robots: { index: false, follow: false },
+}
+
+export default function AccountLayout({ children }) {
+    return children
+}
